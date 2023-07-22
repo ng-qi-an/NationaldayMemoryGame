1e41bb940d5bb5285a786062ab2497451952aa3e
Pop Plays <ng.qi.an@sji.edu.sg>
2023-07-22 10:12:26 +0800

    Initial Commit

---
+++ Nationalday/NationaldayApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationaldayApp.swift
+//  Nationalday
+//
+//  Created by Ng Qi An on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct NationaldayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
